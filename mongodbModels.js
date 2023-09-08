0d69f5b64b440c0a430fbbf4fb2cc92077b00b56
munemu82 <munemu82@gmail.com>
2023-09-08 16:49:21 +1000

    Update mongodbModels.js

--- mongodbModels.js
+++ mongodbModels.js
@@ -60,3 +60,54 @@ const newItem = new Item({
 res.status(400).send({message: "error"})
 }
 })
+
+//FETCH ONE 
+    router.get('/items/:id', async(req, res) => {
+try{
+   const item = await Item.findOne({_id: req.params.id})
+if(!item) {
+   res.status(404).send({error: "Item not found"})
+}
+   res.status(200).send(item)
+} catch (error) {
+   res.status(400).send(error)
+}
+})
+
+//Fetch all items
+    router.get('/items', async(req, res) => {
+try {
+  const items = await Item.find({})
+  res.status(200).send(items)
+} catch (error) {
+  res.status(400).send(error)
+}
+})
+
+//UPDATE ITEM
+    router.patch('/items/:id', Auth, async(req, res) => {
+const updates = Object.keys(req.body)
+const allowedUpdates = ['name', 'description', 'category', 'price']
+const isValidOperation = updates.every((update) =>              allowedUpdates.includes(update))
+   if(!isValidOperation) {
+     return res.status(400).send({ error: 'invalid updates'})
+}
+try {
+  const item = await Item.findOne({ _id: req.params.id})
+  if(!item){
+      return res.status(404).send()
+  }
+  updates.forEach((update) => item[update] = req.body[update])
+  await item.save()
+  res.send(item)
+} catch (error) {
+res.status(400).send(error)
+}
+})
+
+
+-----------------------------------------
+  TOP BEST AND FREE APIs
+-----------------------------------------
+  https://tripprivacy.com/free-apis-to-use-and-make-money/?expand_article=1
+    
